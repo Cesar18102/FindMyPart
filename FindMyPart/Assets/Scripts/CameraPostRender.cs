8d5271ac1476112409595818c0da09580a0ccf0a
Cesar18102 <cesar18102000@gmail.com>
2021-01-30 13:29:19 +0200

    scripting init

---
+++ FindMyPart/Assets/Scripts/CameraPostRender.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class CameraPostRender : MonoBehaviour {
+    private Camera _Camera { get; set; }
+    private Shader _Shader { get; set; }
+
+	// Use this for initialization
+	void Start () {
+        _Camera = this.GetComponent<Camera>();
+        _Shader = Shader.Find("Custom/ColoredWatchShader");
+
+        Shader.SetGlobalColor(Shader.PropertyToID("_Light"), new Color(1.0f, 0.0f, 0.0f, 0.1f));
+        _Camera.SetReplacementShader(_Shader, null);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
---
+++ FindMyPart/Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+public class GameController : MonoBehaviour {
+
+    public GameStateInfo _GameStateInfo { get; set; }
+    public GameObject Player { get; set; }
+    public PlayerController _PlayerController { get; set; }
+
+	// Use this for initialization
+	void Start () {
+        _GameStateInfo = new GameStateInfo();
+        _GameStateInfo._PlayerStateInfo = new PlayerStateInfo();
+
+        Player = GameObject.Find("Player");
+        _PlayerController = Player.GetComponent<PlayerController>();
+        _PlayerController._PlayerStateInfo = _GameStateInfo._PlayerStateInfo;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
